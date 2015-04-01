0fb43409649f99a8db2a4466dbe3d18758470e6f
Gary Morris <garym@GaryM-Mac-Mini-2.local>
2015-04-01 06:13:40 -0700

    English version of app completed.

--- BigProject/ViewController.m
+++ BigProject/ViewController.m
@@ -17,6 +17,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UILabel* label3 = [[UILabel alloc] init];
+    label3.text = NSLocalizedString(@"This is label number 3 from code", @"keep it simple");
+    label3.frame = CGRectMake(20,40,320,25);
+    label3.numberOfLines = 0;
+    label3.adjustsFontSizeToFitWidth = YES;
+    label3.minimumScaleFactor = 0.66f;
+    label3.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:label3];
+    
+    for (UIView* v in self.view.subviews) {
+        v.layer.borderColor = [UIColor greenColor].CGColor;
+        v.layer.borderWidth = 1;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
